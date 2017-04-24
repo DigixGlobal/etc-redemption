8b5a63be39d2f31572f29b5b1f73867073d7a5e4
Chris Hitchcott <hitchcott@gmail.com>
2017-04-24 15:54:58 +0700

    [feature] add `Fund` event, rename `redeemed` to `redeemedOf`

--- contracts/EtcRedemptionToken.sol
+++ contracts/EtcRedemptionToken.sol
@@ -10,6 +10,7 @@ contract EtcRedemptionToken is TimeLockable, ERC20 {
 
   mapping(address => uint) redemptions;
 
+  event Fund(address indexed from, uint value);
   event Redeem(address indexed from, address indexed to, uint value);
 
   function EtcRedemptionToken() {
@@ -17,24 +18,39 @@ contract EtcRedemptionToken is TimeLockable, ERC20 {
   }
 
   function () payable {
-    // prevent non-zero transactions
+    // not really payable; let's prevent accidental sends
     if (msg.value > 0) { throw; }
     redeem(msg.sender);
   }
 
+  // only method that allows funding
+  function fund() payable {
+    Fund(msg.sender, msg.value);
+  }
+
   function redeem(address _to) isActive {
+    // the balance owend to the sender
     var _amount = balances[msg.sender];
+    // throw if the balance is zero
+    if (_amount == 0) { throw; }
     // throw if contract doesn't have enough value
     if(address(this).balance < _amount) { throw; }
+    // set the new balance to zero
     balances[msg.sender] = 0;
+    // update the accounts' redemption amount
     redemptions[msg.sender] = SafeMath.safeAdd(redemptions[msg.sender], _amount);
+    // update the token's total supply
     totalSupply = SafeMath.safeSub(totalSupply, _amount);
+    // update the total redeemed value
     totalRedeemed = SafeMath.safeAdd(totalRedeemed, _amount);
+    // send the wei to the requested address
+    // TODO check for null address?
     if (!_to.send(_amount)) { throw; }
+    // emit event
     Redeem(msg.sender, _to, _amount);
   }
 
-  function redeemed(address _owner) constant returns (uint _amount) {
+  function redeemedOf(address _owner) constant returns (uint _amount) {
     return redemptions[_owner];
   }
 }
